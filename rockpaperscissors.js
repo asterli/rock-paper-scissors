79ca1198a32c8dce6776f579098056ee78d39e3a
aster <asli@scu.edu>
2022-12-17 19:26:44 -0800

    Write getPlayerChoice which prompts user for input

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -10,15 +10,20 @@ function getComputerChoice (){
     //returns rock, paper, or scissors
 }
 
+function getPlayerChoice(){
+    var choice = prompt("Rock, paper, scissors, shoot:");
+    var lowercase = choice.toLowerCase(); //ensures that the user input does not need to be case-sensitive
+    return lowercase;
+}
+
 function gameRound(computerSelection, playerSelection){
-    var playerSelectionLower = playerSelection.toLowerCase();
-    if (computerSelection == 'rock' && playerSelectionLower == 'scissors'){
+    if (computerSelection == 'rock' && playerSelection == 'scissors'){
         console.log(`You Lose! The computer chose: ${computerSelection}, which beats ${playerSelection}. Try again!`);
     } else if (computerSelection == 'scissors' && playerSelectionLower == 'paper'){
         console.log(`You Lose! The computer chose: ${computerSelection}, which beats ${playerSelection}. Try again!`);
-    } else if (computerSelection == 'paper' && playerSelectionLower == 'rock'){
+    } else if (computerSelection == 'paper' && playerSelection == 'rock'){
         console.log(`You Lose! The computer chose: ${computerSelection}, which beats ${playerSelection}. Try again!`);
-    } else if (computerSelection === playerSelectionLower){
+    } else if (computerSelection === playerSelection){
         console.log(`You Tied! The computer chose: ${computerSelection}. You chose: ${playerSelection}. Try again.`);
     } else {
         console.log(`You Win! The computer chose: ${computerSelection}, which loses again ${playerSelection}.`);
