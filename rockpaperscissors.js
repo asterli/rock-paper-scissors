28ab89e605f66e6b5b2c0d540a9bcde50f83bff5
aster <asli@scu.edu>
2022-12-17 19:22:09 -0800

    Write gameRound which simulates a round played

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -9,3 +9,18 @@ function getComputerChoice (){
     //case 1: rock, etc...
     //returns rock, paper, or scissors
 }
+
+function gameRound(computerSelection, playerSelection){
+    var playerSelectionLower = playerSelection.toLowerCase();
+    if (computerSelection == 'rock' && playerSelectionLower == 'scissors'){
+        console.log(`You Lose! The computer chose: ${computerSelection}, which beats ${playerSelection}. Try again!`);
+    } else if (computerSelection == 'scissors' && playerSelectionLower == 'paper'){
+        console.log(`You Lose! The computer chose: ${computerSelection}, which beats ${playerSelection}. Try again!`);
+    } else if (computerSelection == 'paper' && playerSelectionLower == 'rock'){
+        console.log(`You Lose! The computer chose: ${computerSelection}, which beats ${playerSelection}. Try again!`);
+    } else if (computerSelection === playerSelectionLower){
+        console.log(`You Tied! The computer chose: ${computerSelection}. You chose: ${playerSelection}. Try again.`);
+    } else {
+        console.log(`You Win! The computer chose: ${computerSelection}, which loses again ${playerSelection}.`);
+    }
+}
