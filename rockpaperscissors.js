1fed79daaec5ab02e8a0ff529d7c04217e992672
aster <asli@scu.edu>
2022-12-17 19:07:26 -0800

    Write pseudocode for getComputerChoice function

---
+++ rockpaperscissors.js
@@ -0,0 +1,11 @@
+//Functions
+
+function getComputerChoice (){
+    const rock = "rock";
+    const paper = "paper";
+    const scissors = "scissors";
+    //randomly generate # 1-3
+    //switch case (num)
+    //case 1: rock, etc...
+    //returns rock, paper, or scissors
+}
